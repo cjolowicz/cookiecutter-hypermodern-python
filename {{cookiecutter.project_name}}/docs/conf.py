9c3129c28f85cfb4faad8f2d48d8ce65f58c63ee
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-02-07 16:32:45 +0100

    Remove custom CSS

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -6,4 +6,3 @@ project = "{{cookiecutter.friendly_name}}"
 author = "{{cookiecutter.author}}"
 copyright = f"{datetime.now().year}, {author}"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_autodoc_typehints"]
-html_static_path = ["_static"]
