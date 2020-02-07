9918a9c90e01ebeaa1b392de3dcec9613ef0cd05
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-02-07 16:34:07 +0100

    Remove unused conftest.py

--- {{cookiecutter.project_name}}/tests/conftest.py
+++
@@ -1,7 +0,0 @@
-"""Package-wide test fixtures."""
-from _pytest.config import Config
-
-
-def pytest_configure(config: Config) -> None:
-    """Pytest configuration hook."""
-    config.addinivalue_line("markers", "e2e: mark as end-to-end test.")
