85c2480fac14045581dbec9d5cc99703cbeba831
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-03-03 06:52:48 +0100

    [nox] Remove build directory before invoking Sphinx

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,7 @@
 """Nox sessions."""
+from pathlib import Path
+import shutil
+
 import nox
 from nox.sessions import Session
 
@@ -6,5 +9,8 @@ from nox.sessions import Session
 @nox.session
 def docs(session: Session) -> None:
     """Build the documentation."""
+    builddir = Path("docs", "_build")
+    if builddir.exists():
+        shutil.rmtree(builddir)
     session.install("sphinx", "recommonmark")
     session.run("sphinx-build", "docs", "docs/_build")
