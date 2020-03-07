44c3bbf3f8a8d11b0b7acb70c31f150335dfd7e9
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-03-07 22:38:04 +0100

    [conf] Add GitHub banner and button

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,12 @@ author = "Claudio Jolowicz"
 copyright = f"{datetime.now().year}, {author}"
 extensions = ["recommonmark"]
 html_theme_options = {
+    "github_banner": "true",
+    "github_button": "true",
+    "github_count": "true",
+    "github_user": "cjolowicz",
+    "github_repo": "cookiecutter-hypermodern-python",
+    "github_type": "star",
     "logo": "../../Logo.png",
     "logo_name": True,
     "fixed_sidebar": True,
