c97383db8d50a33270432956342b73336a77e804
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-03-08 21:39:17 +0100

    Support Python 3.9 in Nox sessions
    cjolowicz/cookiecutter-hypermodern-python-instance#57

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -8,7 +8,7 @@ from nox.sessions import Session
 
 
 package = "{{cookiecutter.package_name}}"
-python_versions = ["3.8", "3.7", "3.6"]
+python_versions = ["3.9", "3.8", "3.7", "3.6"]
 nox.options.sessions = "lint", "safety", "mypy", "pytype", "tests"
 locations = "src", "tests", "noxfile.py", "docs/conf.py"
 
