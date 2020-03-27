d405e9e8ed0414f9e3d243817e4a967951b18776
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-03-27 23:02:25 +0100

    [conf] Add Sphinx configuration

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+"""Sphinx configuration."""
+from datetime import datetime
+
+
+project = "Hypermodern Python Cookiecutter"
+author = "Claudio Jolowicz"
+copyright = f"{datetime.now().year}, {author}"
+extensions = ["recommonmark"]
