ccbe79f41a50ec3804ba21ae22c7e1a2eed46427
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-03-28 19:39:53 +0100

    [nox] Use sphinx-autobuild for interactive sessions

--- noxfile.py
+++ noxfile.py
@@ -9,8 +9,18 @@ from nox.sessions import Session
 @nox.session
 def docs(session: Session) -> None:
     """Build the documentation."""
+    args = session.posargs or ["docs", "docs/_build"]
+
+    if session.interactive and not session.posargs:
+        args.append("--open-browser")
+
     builddir = Path("docs", "_build")
     if builddir.exists():
         shutil.rmtree(builddir)
-    session.install("sphinx", "recommonmark")
-    session.run("sphinx-build", "docs", "docs/_build")
+
+    session.install("sphinx", "sphinx-autobuild", "recommonmark")
+
+    if session.interactive:
+        session.run("sphinx-autobuild", *args)
+    else:
+        session.run("sphinx-build", *args)
