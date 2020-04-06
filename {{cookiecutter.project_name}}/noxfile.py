a411937c033694361ac84c5e4ab5595690da27aa
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-04-06 23:29:02 +0200

    Allow passing arguments to sphinx-{build,autobuild}

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -197,6 +197,11 @@ def coverage(session: Session) -> None:
 @nox.session(python="3.8")
 def docs(session: Session) -> None:
     """Build the documentation."""
+    args = session.posargs or ["docs", "docs/_build"]
+
+    if session.interactive and not session.posargs:
+        args.insert(0, "--open-browser")
+
     builddir = Path("docs", "_build")
     if builddir.exists():
         shutil.rmtree(builddir)
@@ -210,9 +215,7 @@ def docs(session: Session) -> None:
         "sphinx-autodoc-typehints",
     )
 
-    args = ["docs", "docs/_build"]
-
     if session.interactive:
-        session.run("sphinx-autobuild", "--open-browser", *args)
+        session.run("sphinx-autobuild", *args)
     else:
         session.run("sphinx-build", *args)
