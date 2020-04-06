3e33e4c5af62c6ee4447aab18610e7e117d4c797
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-04-06 23:11:49 +0200

    Use sphinx-autobuild for interactive sessions

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -200,6 +200,19 @@ def docs(session: Session) -> None:
     builddir = Path("docs", "_build")
     if builddir.exists():
         shutil.rmtree(builddir)
+
     install_package(session)
-    install(session, "recommonmark", "sphinx", "sphinx-autodoc-typehints")
-    session.run("sphinx-build", "docs", "docs/_build")
+    install(
+        session,
+        "recommonmark",
+        "sphinx",
+        "sphinx-autobuild",
+        "sphinx-autodoc-typehints",
+    )
+
+    args = ["docs", "docs/_build"]
+
+    if session.interactive:
+        session.run("sphinx-autobuild", "--open-browser", *args)
+    else:
+        session.run("sphinx-build", *args)
