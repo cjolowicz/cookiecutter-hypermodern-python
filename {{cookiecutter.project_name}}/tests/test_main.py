5a58a5b4de08e43c4701e8c7a0d75481eedaa424
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-04-19 22:50:59 +0200

    Remove console module (#152)
    * Move console.main to __main__ module
    
    * Remove console module
    
    * Replace test_console by test_main module
    
    * Adapt tool.poetry.scripts to use __main__
    
    * Do not omit __main__ from coverage
    
    * Disable coverage for module-level code in __main__
    
    * Adapt API documentation to use __main__
    
    * Adapt documentation to removal of console module
    
    cjolowicz/cookiecutter-hypermodern-python-instance#111

--- {{cookiecutter.project_name}}/tests/test_main.py
+++ {{cookiecutter.project_name}}/tests/test_main.py
@@ -1,8 +1,8 @@
-"""Test cases for the console module."""
+"""Test cases for the __main__ module."""
 from click.testing import CliRunner
 import pytest
 
-from {{cookiecutter.package_name}} import console
+from {{cookiecutter.package_name}} import __main__
 
 
 @pytest.fixture
@@ -13,5 +13,5 @@ def runner() -> CliRunner:
 
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
-    result = runner.invoke(console.main)
+    result = runner.invoke(__main__.main)
     assert result.exit_code == 0
