0cb0046e982f14c5d72e14115248b20095ce8499
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-04-20 08:02:03 +0200

    Remove __version__
    * Do not pass version to click.version_option
    
    By default, click.version_option determines the version using the installed
    package metadata. This is precisely where we get out __version__ from, so there
    is no point passing it to version_option explicitly.
    
    * Remove __version__ from the package
    
    There is no need to define __version__ up front. Users have importlib.metadata
    if they need the version number.
    
    See https://hynek.me/articles/packaging-metadata/
    
    * Remove importlib-metadata from dependencies
    
    * Remove importlib-metadata from mypy.ini
    
    cjolowicz/cookiecutter-hypermodern-python-instance#112

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -1,13 +1,1 @@
 """{{cookiecutter.friendly_name}}."""
-import sys
-
-if sys.version_info[:2] >= (3, 8):
-    from importlib.metadata import version, PackageNotFoundError  # pragma: no cover
-else:
-    from importlib_metadata import version, PackageNotFoundError  # pragma: no cover
-
-
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
