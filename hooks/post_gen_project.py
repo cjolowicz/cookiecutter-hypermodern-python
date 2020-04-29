d4fb1e81dcf5dd5af77f8a371586d24d8f6a0ade
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-04-29 16:20:34 +0200

    Add post generation hook to fix JSON indentation width

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import json
+from pathlib import Path
+
+
+def reindent_cookiecutter_json():
+    """Indent .cookiecutter.json using two spaces.
+
+    The jsonify extension distributed with Cookiecutter uses an indentation
+    width of four spaces. This conflicts with the default indentation width of
+    Prettier for JSON files. Prettier is run as a pre-commit hook in CI.
+    """
+    path = Path(".cookiecutter.json")
+
+    with path.open() as io:
+        data = json.load(io)
+
+    with path.open(mode="w") as io:
+        json.dump(data, io, sort_keys=True, indent=2)
+        io.write("\n")
+
+
+if __name__ == "__main__":
+    reindent_cookiecutter_json()
