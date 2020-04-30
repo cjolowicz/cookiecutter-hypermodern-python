b3496e47ba8314455edc88d3ef3744424ac3cdfa
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-04-30 08:37:31 +0200

    Do not install package in mypy session (#207)
    cjolowicz/cookiecutter-hypermodern-python-instance#143

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -149,7 +149,6 @@ def safety(session: Session) -> None:
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or locations
-    install_package(session)
     install(session, "mypy")
     session.run("mypy", *args)
 
