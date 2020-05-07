834215db11f2bdfb2f4c2d56cffd265eda0f2ca6
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-05-07 12:29:58 +0200

    Remove flake8-import-order (#228)
    * Remove flake8-import-order
    
    * Remove flake8-import-order from .flake8
    
    * Remove flake8-import-order from Nox session
    
    * Remove flake8-import-order from development dependencies
    
    * Remove flake8-import-order from documentation

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -128,7 +128,6 @@ def lint(session: Session) -> None:
         "flake8-black",
         "flake8-bugbear",
         "flake8-docstrings",
-        "flake8-import-order",
         "flake8-rst-docstrings",
         "pep8-naming",
         "darglint",
