affd1956e1d185429c0d3daac3d3b5b2fb6bc0ce
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-07-15 13:56:26 +0200

    Track pre-commit hooks as Poetry dependencies (#397)
    * Add flake8 3.8.3 (dev)
    
    * Add black 19.10b0 (dev)
    
    * Add flake8-bandit 2.1.2 (dev)
    
    * Add flake8-bugbear 20.1.4 (dev)
    
    * Add flake8-docstrings 1.5.0 (dev)
    
    * Add flake8-rst-docstrings 0.0.13 (dev)
    
    * Add pep8-naming 0.10.0 (dev)
    
    * Add darglint 1.4.1 (dev)
    
    * Add reorder-python-imports 2.3.1 (dev)
    
    * Add pre-commit-hooks 3.1.0 (dev)
    
    * Install hook dependencies into pre-commit session
    
    * Use repository-local hook for flake8
    
    * Use repository-local hook for black
    
    * Use repository-local hook for reorder-python-imports
    
    * Sort repository-local hooks
    
    * Use repository-local hook for check-toml
    
    * Use repository-local hook for check-yaml
    
    * Use repository-local hook for end-of-file-fixer
    
    * Use repository-local hook for trailing-whitespace
    
    * Use repository-local hook for check-added-large-files
    
    * Update poetry.lock
    
    * Activate virtualenv in pre-commit hooks
    
    * Update documentation for pre-commit

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -4,6 +4,7 @@ import shutil
 import sys
 import tempfile
 from pathlib import Path
+from textwrap import dedent
 from typing import cast
 from typing import Iterator
 
@@ -110,12 +111,78 @@ def install(session: Session, *args: str) -> None:
         session.install(f"--constraint={requirements}", *args)
 
 
+def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
+    """Activate virtualenv in hooks installed by pre-commit.
+
+    This function patches git hooks installed by pre-commit to activate the
+    session's virtual environment. This allows pre-commit to locate hooks in
+    that environment when invoked from git.
+
+    Args:
+        session: The Session object.
+    """
+    if session.bin is None:
+        return
+
+    virtualenv = session.env.get("VIRTUAL_ENV")
+    if virtualenv is None:
+        return
+
+    hookdir = Path(".git") / "hooks"
+    if not hookdir.is_dir():
+        return
+
+    for hook in hookdir.iterdir():
+        if hook.name.endswith(".sample") or not hook.is_file():
+            continue
+
+        text = hook.read_text()
+        bindir = repr(session.bin)[1:-1]  # strip quotes
+        if not (
+            Path("A") == Path("a") and bindir.lower() in text.lower() or bindir in text
+        ):
+            continue
+
+        lines = text.splitlines()
+        if not (lines[0].startswith("#!") and "python" in lines[0].lower()):
+            continue
+
+        header = dedent(
+            f"""\
+            import os
+            os.environ["VIRTUAL_ENV"] = {virtualenv!r}
+            os.environ["PATH"] = os.pathsep.join((
+                {session.bin!r},
+                os.environ.get("PATH", ""),
+            ))
+            """
+        )
+
+        lines.insert(1, header)
+        hook.write_text("\n".join(lines))
+
+
 @nox.session(name="pre-commit", python="3.8")
 def precommit(session: Session) -> None:
     """Lint using pre-commit."""
     args = session.posargs or ["run", "--all-files", "--show-diff-on-failure"]
-    install(session, "pre-commit")
+    install(
+        session,
+        "black",
+        "darglint",
+        "flake8",
+        "flake8-bandit",
+        "flake8-bugbear",
+        "flake8-docstrings",
+        "flake8-rst-docstrings",
+        "pep8-naming",
+        "pre-commit",
+        "pre-commit-hooks",
+        "reorder-python-imports",
+    )
     session.run("pre-commit", *args)
+    if args and args[0] == "install":
+        activate_virtualenv_in_precommit_hooks(session)
 
 
 @nox.session(python="3.8")
