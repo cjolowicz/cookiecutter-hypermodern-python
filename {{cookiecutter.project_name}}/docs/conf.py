a3aa5640f29386d4fce11d01a2614de8bc2bb2f5
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-10-10 00:45:32 +0200

    Configure Sphinx to use sphinx-click

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -5,6 +5,11 @@ from datetime import datetime
 project = "{{cookiecutter.friendly_name}}"
 author = "{{cookiecutter.author}}"
 copyright = f"{datetime.now().year}, {author}"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_click",
+    "sphinx_rtd_theme",
+]
 autodoc_typehints = "description"
 html_theme = "sphinx_rtd_theme"
