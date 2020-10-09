5c053cddbe631fd2fa92957acb9015d30a990875
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-10-09 18:53:22 +0200

    Configure Sphinx to use sphinx-rtd-theme

--- {{cookiecutter.project_name}}/docs/conf.py
+++ {{cookiecutter.project_name}}/docs/conf.py
@@ -5,5 +5,6 @@ from datetime import datetime
 project = "{{cookiecutter.friendly_name}}"
 author = "{{cookiecutter.author}}"
 copyright = f"{datetime.now().year}, {author}"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_rtd_theme"]
 autodoc_typehints = "description"
+html_theme = "sphinx_rtd_theme"
