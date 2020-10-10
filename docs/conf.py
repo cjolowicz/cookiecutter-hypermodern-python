d09797aae9295e8be850ea2a9b0cc0dcbe8a1b54
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-10-10 21:14:18 +0200

    Configure Sphinx to use furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -8,20 +8,8 @@ copyright = f"{datetime.now().year}, {author}"
 extensions = ["sphinx.ext.intersphinx"]
 intersphinx_mapping = {"mypy": ("https://mypy.readthedocs.io/en/stable/", None)}
 language = "en"
-html_static_path = ["_static"]
-html_theme = "alabaster"
-html_theme_options = {
-    "github_banner": "true",
-    "github_button": "true",
-    "github_count": "true",
-    "github_user": "cjolowicz",
-    "github_repo": "cookiecutter-hypermodern-python",
-    "github_type": "star",
-    "logo": "logo.png",
-    "logo_name": "true",
-    "fixed_sidebar": "true",
-    "sidebar_width": "250px",
-}
+html_theme = "furo"
+html_logo = "_static/logo.png"
 linkcheck_ignore = [
     "codeofconduct.html",
     "https://github.com/PyCQA/flake8-bugbear#",
