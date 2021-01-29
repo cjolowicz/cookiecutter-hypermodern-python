9b0450ec5738c4ecd3d4d4a98dea1431b58147e1
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-01-29 13:19:42 +0100

    Add tools to automate Cookiecutter releases (#674)
    * Add tool for preparing a GitHub release
    
    * Add tool for publishing the GitHub release
    
    * Add Nox sessions to prepare and publish Cookiecutter releases

--- noxfile.py
+++ noxfile.py
@@ -5,6 +5,33 @@ import shutil
 import nox
 from nox.sessions import Session
 
+nox.options.sessions = ["docs"]
+owner, repository = "cjolowicz", "cookiecutter-hypermodern-python"
+labels = "cookiecutter", "documentation"
+bump_paths = "README.rst", "docs/guide.rst", "docs/index.rst", "docs/quickstart.rst"
+
+
+@nox.session(name="prepare-release")
+def prepare_release(session: Session) -> None:
+    """Prepare a GitHub release."""
+    args = [
+        f"--owner={owner}",
+        f"--repository={repository}",
+        *[f"--bump={path}" for path in bump_paths],
+        *[f"--label={label}" for label in labels],
+        *session.posargs,
+    ]
+    session.install("click", "github3")
+    session.run("tools/prepare-github-release.py", *args, external=True)
+
+
+@nox.session(name="publish-release")
+def publish_release(session: Session) -> None:
+    """Publish a GitHub release."""
+    args = [f"--owner={owner}", f"--repository={repository}", *session.posargs]
+    session.install("click", "github3")
+    session.run("tools/publish-github-release.py", *args, external=True)
+
 
 @nox.session
 def docs(session: Session) -> None:
