f9e4639233dd49f5c5c2cfec419deb558e51e32b
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-15 17:32:31 +0200

    Improve error message when nox-poetry is not installed

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -5,8 +5,18 @@ from pathlib import Path
 from textwrap import dedent
 
 import nox
-from nox_poetry import Session
-from nox_poetry import session
+
+try:
+    from nox_poetry import Session
+    from nox_poetry import session
+except ImportError:
+    message = f"""\
+    Nox failed to import the 'nox-poetry' package.
+
+    Please install it using the following command:
+
+    {sys.executable} -m pip install nox-poetry"""
+    raise SystemExit(dedent(message))
 
 
 package = "{{cookiecutter.package_name}}"
