e3ad6fbd6b775a9386356652c79b9fb4667919de
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-04-15 15:55:27 +0200

    Print full report in safety session (#872)
    Currently, the safety session uses --bare which prints only the names of
    vulnerable packages, without providing additional information, such as the
    Safety DB identifier and the affected version ranges. Use --full-report instead.

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -100,7 +100,7 @@ def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
     requirements = session.poetry.export_requirements()
     session.install("safety")
-    session.run("safety", "check", f"--file={requirements}", "--bare")
+    session.run("safety", "check", "--full-report", f"--file={requirements}")
 
 
 @session(python=python_versions)
