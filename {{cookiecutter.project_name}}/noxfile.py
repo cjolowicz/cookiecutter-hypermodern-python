66364198245fc85ded1c3acabbde459d8956b301
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-11 11:01:29 +0200

    :construction_worker: [nox] Add version requirement >= 2021.6.6

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -21,6 +21,7 @@ except ImportError:
 
 package = "{{cookiecutter.package_name}}"
 python_versions = ["3.9", "3.8", "3.7", "3.6"]
+nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
     "safety",
