a9019283e9089a10624c30ba18353f5415508614
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-16 13:10:05 +0200

    Remove types-click from Nox session for mypy (#952)
    * Remove types-click from Nox session for mypy
    
    click >= 8.0 ships with py.typed, so types-click is not required.
    
    Installing types-click with click >= 8.0 leads to a mypy error:
    
    - https://github.com/python/typeshed/issues/5642
    
    * Remove unused `type: ignore` on `@click.version_option`

--- {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.package_name}}/__main__.py
@@ -2,12 +2,8 @@
 import click
 
 
-# @click.version_option results in "untyped decorator" error
-# https://github.com/python/typeshed/issues/5642
-
-
 @click.command()
-@click.version_option()  # type: ignore[misc]
+@click.version_option()
 def main() -> None:
     """{{cookiecutter.friendly_name}}."""
 
