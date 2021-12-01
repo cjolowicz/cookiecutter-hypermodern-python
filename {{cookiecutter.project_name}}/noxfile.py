df8f583266e1c71128b5403ef58a272f6b1d2499
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-12-01 08:13:31 +0100

    ✨ [nox] Support patching pre-commit hooks written in bash
    * 🔨 Split conditional
    
    * 🔨 Replace `if not continue` with `if`
    
    * ✨ Add bash version
    
    * 🔨 Add comments with pre-commit version ranges
    
    * 🔨 Extract variable `patches`
    
    * :hammer: Move bash fragment into `patches`
    
    * 🔨 Slide comments
    
    * :hammer: Replace `if...elif` with `for...if...break` loop over `patches`
    
    * 🔨 Do not duplicate `patches` keys
    
    * 🔨 Inline variable `header`
    
    * 🔨 Rename variable `patches` to `headers`
    
    * 🔨 Rename variable `patch` to `header`
    
    * :hammer: Support multiple languages when checking for bindir
    
    * ✨ Support bash when checking for bindir
    
    * 🔨 Slide `bindirs` before loop
    
    * 🐛 Fix over-aggressive quote removal for bindir in bash hooks
    
    * 🔨 Inline variable `bindirs` into `bindirs`
    
    * 💡 Expand comment
    
    * 🔨 Slide `headers` before loop
    
    * 🔨 Replace `lines[0].startswith` with `hook.read_bytes().startswith`
    
    * 🐛 Do not attempt to decode hooks unless they have a shebang
    
    Retrocookie-Original-Commit: cjolowicz/cookiecutter-hypermodern-python-instance@b2a958a

--- {{cookiecutter.project_name}}/noxfile.py
+++ {{cookiecutter.project_name}}/noxfile.py
@@ -1,5 +1,6 @@
 """Nox sessions."""
 import os
+import shlex
 import shutil
 import sys
 from pathlib import Path
@@ -46,9 +47,34 @@ def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
     """
     assert session.bin is not None  # noqa: S101
 
+    # Only patch hooks containing a reference to this session's bindir. Support
+    # quoting rules for Python and bash, but strip the outermost quotes so we
+    # can detect paths within the bindir, like <bindir>/python.
+    bindirs = [
+        bindir[1:-1] if bindir[0] in "'\"" else bindir
+        for bindir in (repr(session.bin), shlex.quote(session.bin))
+    ]
+
     virtualenv = session.env.get("VIRTUAL_ENV")
     if virtualenv is None:
         return
+
+    headers = {
+        # pre-commit < 2.16.0
+        "python": f"""\
+            import os
+            os.environ["VIRTUAL_ENV"] = {virtualenv!r}
+            os.environ["PATH"] = os.pathsep.join((
+                {session.bin!r},
+                os.environ.get("PATH", ""),
+            ))
+            """,
+        # pre-commit >= 2.16.0
+        "bash": f"""\
+            VIRTUAL_ENV={shlex.quote(virtualenv)}
+            PATH={shlex.quote(session.bin)}{os.pathsep}"$PATH"
+            """,
+    }
 
     hookdir = Path(".git") / "hooks"
     if not hookdir.is_dir():
@@ -58,30 +84,24 @@ def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
         if hook.name.endswith(".sample") or not hook.is_file():
             continue
 
+        if not hook.read_bytes().startswith(b"#!"):
+            continue
+
         text = hook.read_text()
-        bindir = repr(session.bin)[1:-1]  # strip quotes
-        if not (
+
+        if not any(
             Path("A") == Path("a") and bindir.lower() in text.lower() or bindir in text
+            for bindir in bindirs
         ):
             continue
 
         lines = text.splitlines()
-        if not (lines[0].startswith("#!") and "python" in lines[0].lower()):
-            continue
 
-        header = dedent(
-            f"""\
-            import os
-            os.environ["VIRTUAL_ENV"] = {virtualenv!r}
-            os.environ["PATH"] = os.pathsep.join((
-                {session.bin!r},
-                os.environ.get("PATH", ""),
-            ))
-            """
-        )
-
-        lines.insert(1, header)
-        hook.write_text("\n".join(lines))
+        for executable, header in headers.items():
+            if executable in lines[0].lower():
+                lines.insert(1, dedent(header))
+                hook.write_text("\n".join(lines))
+                break
 
 
 @session(name="pre-commit", python=python_versions[0])
