644b3dfb92cdc634f75bea4803649d9833dd1dbe
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-04-19 10:07:04 +0200

    pre-commit

--- {{cookiecutter.project_name}}/docs/gen_ref_pages.py
+++ {{cookiecutter.project_name}}/docs/gen_ref_pages.py
@@ -4,22 +4,22 @@ from pathlib import Path
 
 import mkdocs_gen_files
 
-for path in sorted(Path("src").rglob("*.py")):  # 
-    module_path = path.relative_to("src").with_suffix("")  # 
-    doc_path = path.relative_to("src").with_suffix(".md")  # 
-    full_doc_path = Path("reference", doc_path)  # 
+for path in sorted(Path("src").rglob("*.py")):
+    module_path = path.relative_to("src").with_suffix("")
+    doc_path = path.relative_to("src").with_suffix(".md")
+    full_doc_path = Path("reference", doc_path)
 
     parts = list(module_path.parts)
 
-    if parts[-1] == "__init__":  # 
+    if parts[-1] == "__init__":
         parts = parts[:-1]
         doc_path = doc_path.with_name("index.md")
         full_doc_path = full_doc_path.with_name("index.md")
     elif parts[-1] == "__main__":
         continue
 
-    with mkdocs_gen_files.open(full_doc_path, "w") as fd:  # 
-        identifier = ".".join(parts)  # 
-        print("::: " + identifier, file=fd)  # 
+    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+        identifier = ".".join(parts)
+        print("::: " + identifier, file=fd)
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, path)  # 
+    mkdocs_gen_files.set_edit_path(full_doc_path, path)
